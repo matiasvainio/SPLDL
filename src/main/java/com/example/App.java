bfe62f37126f0ab8159b8ade9ba97791a4685636
Matias Vainio <matias.s.vainio@gmail.com>
2021-05-27 13:49:23 +0300

    changed from gson to jackson

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,14 @@
 package com.example;
 
+import com.example.utils.Credentials;
+import com.example.utils.FileHandler;
+
 public class App {
     public static void main(String[] args) {
+        FileHandler fileHandler = new FileHandler();
+        Credentials credentials = fileHandler.read();
+
+        System.out.println(credentials);
+
     }
 }
--- src/main/java/com/example/utils/FileHandler.java
+++ src/main/java/com/example/utils/FileHandler.java
@@ -7,17 +7,19 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FileHandler {
-    private final Gson gson;
+    private ObjectMapper objectMapper;
 
     public FileHandler() {
-        gson = new Gson();
+        objectMapper = new ObjectMapper();
     }
 
     public Credentials read() {
+        Credentials credentials = new Credentials();
         InputStream is = getClass().getClassLoader().getResourceAsStream("credentials.json");
 
         StringBuilder sb = new StringBuilder();
@@ -30,10 +32,16 @@ public class FileHandler {
             e.printStackTrace();
         }
 
-        return deserialize(sb.toString());
+        try {
+            credentials = deserialize(sb.toString());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return credentials;
     }
 
-    public Credentials deserialize(String s) {
-        return gson.fromJson(s, Credentials.class);
+    public Credentials deserialize(String s) throws JsonMappingException, JsonProcessingException {
+        return objectMapper.readValue(s, Credentials.class);
     }
 }
